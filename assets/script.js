4ed04d4476f84b69278600d1c523673212a327aa
austintynes <austintynes10@gmail.com>
2023-10-02 15:51:43 -0500

    added api key, DOM elements, event listeners(search button/ history list)

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const APIkey = "10253967aeb21b7d078f9ef409a75de7";
-    const searchBtn = document.getElementById('search-btn');
-    const cityInput = document.getElementById('city-input');
-    const historyList = document.getElementById('history-list');
-    const weatherContainer = document.getElementById('weather-container');
+// Waits for all DOM elements to load before executing
+document.addEventListener("DOMContentLoaded", function () {
+  // API key
+  const APIkey = "10253967aeb21b7d078f9ef409a75de7";
 
-    searchBtn.addEventListener('click', function () {
-        const city = cityInput.value;
-        if (city) {
-            getWeatherData(city);
-        }
+  // Gets DOM elements
+  const searchBtn = document.getElementById("search-btn");
+  const cityInput = document.getElementById("city-input");
+  const historyList = document.getElementById("history-list");
+  const weatherContainer = document.getElementById("weather-container");
 
-    })
+  // Adds event listener to the search button
+  searchBtn.addEventListener("click", function () {
+    // Gets the city input value
+    const city = cityInput.value;
+    // Checks to see if the input is not empty
+    if (city) {
+      // Call function to get weather data for city
+      getWeatherData(city);
+    }
+  });
 
-
-
-
-
-
-
-})
+  // Adds event listener to history list
+  historyList.addEventListener("click", function (event) {
+    // Checks if item was clicked
+    if (event.target.classList.contains("history-item")) {
+      // Get the text content of clicked city in history
+      const city = event.target.textContent;
+      // Call function to get weather for clicked city in history
+      getweatherData(city);
+    }
+  });
+});
