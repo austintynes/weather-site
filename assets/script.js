cbfb1fce64c1e9d4bef38fca2b376385b3d96214
austintynes <austintynes10@gmail.com>
2023-10-03 21:52:11 -0500

    functional current weather/ 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,42 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 const cityForm = document.getElementById("city-form");
 const cityInput = document.getElementById("city-input");
 const searchHistory = document.getElementById("search-history");
-const currentWeatherDiv = document.getElementById("current-weather");
-const forecastDiv = document.getElementById("forecast");
+const currentWeather = document.getElementById("current-weather");
+const forecast = document.getElementById("forecast");
 
 cityForm.addEventListener("submit", function (event) {
-  const cityName = cityInput.value.trim();
+  let cityName = cityInput.value.trim();
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&appid=" +
-    API_KEY;
+    API_KEY +
+    "&units=imperial";
+  event.preventDefault();
+
+  if (cityName === "") return;
+
+  // Fetch current weather
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Display current weather
+
+      document.querySelector(".city-name").textContent = data.name;
+      document.querySelector(".temp").textContent = data.main.temp;
+      document.querySelector(".wind").textContent = data.wind.speed;
+      document.querySelector(".humidity").textContent = data.main.humidity;
+
+      console.log(data);
+      return;
+    });
+
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    API_KEY +
+    "&units=imperial";
   event.preventDefault();
 
   if (cityName === "") return;
@@ -25,19 +51,27 @@ cityForm.addEventListener("submit", function (event) {
       // Display current weather
       //         displayCurrentWeather(data);
       // Fetch 5-day forecast
+      document.querySelector(".temp1").textContent = data.list[3].main.temp;
+      document.querySelector(".wind1").textContent = data.list[3].wind.speed;
+      document.querySelector(".humidity1").textContent = data.list[3].main.humidity;
+
+      document.querySelector(".temp2").textContent = data.list[11].main.temp;
+      document.querySelector(".wind2").textContent = data.list[11].wind.speed;
+      document.querySelector(".humidity2").textContent = data.list[11].main.humidity;
+
+      document.querySelector(".temp3").textContent = data.list[19].main.temp;
+      document.querySelector(".wind3").textContent = data.list[19].wind.speed;
+      document.querySelector(".humidity3").textContent = data.list[19].main.humidity;
+
+      document.querySelector(".temp4").textContent = data.list[27].main.temp;
+      document.querySelector(".wind4").textContent = data.list[27].wind.speed;
+      document.querySelector(".humidity4").textContent = data.list[27].main.humidity;
+
+      document.querySelector(".temp5").textContent = data.list[35].main.temp;
+      document.querySelector(".wind5").textContent = data.list[35].wind.speed;
+      document.querySelector(".humidity5").textContent = data.list[35].main.humidity;
+
       console.log(data);
       return;
-    })
-    .then((response) => response.json())
-    /*       .then(data => {
-            // Display 5-day forecast
-            displayForecast(data);
-            // Save search history in localStorage
-            saveToLocalStorage(cityName);
-            // Render search history
-            renderSearchHistory();
-        })*/
-    .catch((error) => {
-      console.error("Error fetching data:", error);
     });
 });
